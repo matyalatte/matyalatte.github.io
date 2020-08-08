4e4fee7099e1e84e19d14b5cc27a15f78077e1b5
matyalatte <matyalatte@gmail.com>
2020-08-08 16:28:19 +0900

    add include system

--- include/include.js
+++
@@ -1,9 +0,0 @@
-function include(name){
-    $.ajax({
-        url: name+".html", // 読み込むHTMLファイル
-        cache: false,
-        success: function(html){
-            document.write(html);
-        }
-    });
-}
